761148cb9cdc8dbadec65def615294f61ad209f6
vagison <vagison@gmail.com>
2024-07-01 20:23:29 +0400

    Improved error messages

--- src/constants/messages/error.js
+++ src/constants/messages/error.js
@@ -1,13 +1,13 @@
 const constants = {
   Auth: {
-    ExistingUser: 'User with this phone number or email already exists',
-    NoPhoneNumberOrEmail: 'Please enter a correct phone number or email',
-    InvalidPassword: 'Invalid password provided',
-    InvalidRefreshToken: 'Invalid Refresh token provided',
-    InvalidBearerToken: 'Invalid Bearer token provided',
+    NoPhoneNumberOrEmail: 'Please enter a correct phone number (in international format) or an email',
     PhoneNumberOrEmailRequired: 'Phone number or email is required',
     PasswordRequired: 'Password is required',
     InvalidPasswordLength: 'Password should be at least 6 chars long',
+    ExistingUser: 'User with this phone number or email already exists',
+    InvalidPassword: 'Invalid password provided',
+    InvalidRefreshToken: 'Invalid Refresh token provided',
+    InvalidBearerToken: 'Invalid Bearer token provided',
   },
   User: {
     AccessDenied: 'You are not allowed to access this data or perform this action',
